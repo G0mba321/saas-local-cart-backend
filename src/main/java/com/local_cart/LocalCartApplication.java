a78ef8641f783e46e0bee21e0b66ade4fa5444d8
arte9 <arte9m@gmail.com>
2025-12-04 22:28:37 +0200

    init commit

---
+++ src/main/java/com/local_cart/LocalCartApplication.java
@@ -0,0 +1,13 @@
+package com.local_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalCartApplication.class, args);
+	}
+
+}
