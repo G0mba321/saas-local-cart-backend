9f8bffbe5115d9494057390a8507754351a57897
arte9 <arte9m@gmail.com>
2025-12-09 00:12:47 +0200

    created repositories for entities

---
+++ src/main/java/com/local_cart/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.local_cart.repository;
+
+import com.local_cart.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
