ee1d23259151b082b70174dd38fcefe4a918dcef
arte9 <arte9m@gmail.com>
2025-12-15 00:33:19 +0200

    fixed mistake

--- src/main/java/com/local_cart/repository/ProductRepository.java
+++ src/main/java/com/local_cart/repository/ProductRepository.java
@@ -1,8 +1,9 @@
 package com.local_cart.repository;
 
+import com.local_cart.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductRepository, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
