6324bcef7f09f9369e2060c82e08183068022a6d
arte9 <arte9m@gmail.com>
2026-06-09 22:52:23 +0300

    added logging to catchForgotten method in GlobalExceptionsHandler

--- src/main/java/com/local_cart/exceptions/handlers/GlobalExceptionsHandler.java
+++ src/main/java/com/local_cart/exceptions/handlers/GlobalExceptionsHandler.java
@@ -2,6 +2,7 @@ package com.local_cart.exceptions.handlers;
 
 import com.local_cart.exceptions.ConflictException;
 import com.local_cart.exceptions.ResourceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionsHandler extends ResponseEntityExceptionHandler {
 
@@ -38,6 +39,8 @@ public class GlobalExceptionsHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ProblemDetail catchForgotten(Exception ex) {
+        log.error("exception caught: {}", ex.getMessage(), ex);
+
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
                 "Something went wrong");
 
